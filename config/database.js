dfbc34d705080efbada7397e9c904d757d9b72c7
Daphny Barbosa <daphnyemily@gmail.com>
2021-10-25 11:24:03 -0400

    adding db

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+    'url' : 'mongodb+srv://devdaph:tPshcQuYG0ltFxR9@cluster0.h8uhl.mongodb.net/waterApp?retryWrites=true&w=majority',  
+
+    'dbName': 'waterApp'
+};
